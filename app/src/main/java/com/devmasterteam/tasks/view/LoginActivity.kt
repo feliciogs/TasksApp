f0f22f8db74218c5332be5595c6a2be7b6de9180
Felicio Gabriel <feliciogs@hotmail.com>
2022-09-26 12:54:52 -0300

    Atualizações de interface

--- app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -26,6 +26,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
         // Layout
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         // Eventos
         binding.buttonLogin.setOnClickListener(this)
